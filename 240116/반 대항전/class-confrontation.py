949e73db865347f2da01198e654f356e68ad3a58
April Dayae Keum <dayeyae@gmail.com>
2024-01-16 06:28:36 +0900

    Title: 반 대항전, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240116/반 대항전/class-confrontation.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = []
+for _ in range(4):
+    arr.append(input().split())
+
+max = 0
+winner = ''
+for i in range(4):
+    s = sum(list(map(int, arr[i][1:])))
+    crr_class = arr[i][0][:-1]
+    print('{0} - {1}'.format(crr_class, s))
+    if s > max:
+        winner = crr_class
+
+print('Class {0} is winner!'.format(crr_class))
