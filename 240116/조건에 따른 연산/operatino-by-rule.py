c27a225a9a2759b59a22d72b20cc7c6c46363748
April Dayae Keum <dayeyae@gmail.com>
2024-01-16 18:31:39 +0900

    Title: 조건에 따른 연산, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240116/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt = 0
+
+while(True):
+    if n%2==0:
+        n = (n*3)+1
+    else:
+        n = (n*2)+2
+    cnt = cnt + 1
+    #print(n, cnt)
+    if n >= 1000:
+        break
+    
+
+print(cnt)
