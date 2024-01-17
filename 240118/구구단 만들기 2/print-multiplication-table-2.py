13e5156ef61288a19dbb4a2ba3c6a85f1f0f2a1c
April Dayae Keum <dayeyae@gmail.com>
2024-01-18 03:42:16 +0900

    Title: 구구단 만들기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/구구단 만들기 2/print-multiplication-table-2.py
+++ 240118/구구단 만들기 2/print-multiplication-table-2.py
@@ -4,8 +4,7 @@ row_num = 4
 for i in range(1, row_num+1):
     row_val = i*2
     for j in range(b, a-1, -1):
-        if j == a:
-            print('{} * {} = {}'.format(j, row_val, j*row_val), end='')
-        else:
-            print('{} * {} = {}'.format(j, row_val, j*row_val), end=' / ')
+        print('{} * {} = {}'.format(j, row_val, j*row_val), end='')
+        if j != a:
+            print(' / ', end='')
     print()
