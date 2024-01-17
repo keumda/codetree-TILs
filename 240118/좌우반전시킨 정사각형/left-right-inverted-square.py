36389a1c7a52b176a2d1b0f900dd699554993e22
April Dayae Keum <dayeyae@gmail.com>
2024-01-18 03:06:12 +0900

    Title: 좌우반전시킨 정사각형, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/좌우반전시킨 정사각형/left-right-inverted-square.py
+++ 240118/좌우반전시킨 정사각형/left-right-inverted-square.py
@@ -1,11 +1,7 @@
 n = int(input())
 
-# 1   1 2 3 4 (j+1)
-# 2   1 2 3
-
-
 for i in range(n):
     for j in range(n, 0, -1):
-        curr = (j) * (i + 1)
+        curr = j * (i + 1)
         print(curr, end=' ')
     print()
