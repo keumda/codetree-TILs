9afa156c71444d069429edd0bc50be7b171c99b9
April Dayae Keum <dayeyae@gmail.com>
2024-01-17 18:26:08 +0900

    Title: 체크 다이아몬드, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240117/체크 다이아몬드/check-diamond.py
+++ 240117/체크 다이아몬드/check-diamond.py
@@ -3,14 +3,11 @@ n = int(input())
 max_len = n + (n-1)
 
 for i in range(1, n+1):
-    #print(n, i, n - i)
     star_str = '* '*i
     l_space_num = n - i
     print(l_space_num * ' '+star_str)
 
-# for i in range(n, 0, -1):
-#     star_str = '* '*i
-#     l_space_num = int((max_len - i) / 2)
-#     if i == n:
-#         l_space_num = 0
-#     print(l_space_num*' '+star_str)
+for i in range(1, n):
+    star_str = '* '*(n-i)
+    l_space_num = i
+    print(l_space_num*' '+star_str)
