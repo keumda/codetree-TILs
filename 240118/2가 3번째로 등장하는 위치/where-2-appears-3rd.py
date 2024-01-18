22e0b8364b0134bce874e233ab8ed3acab120fdc
April Dayae Keum <dayeyae@gmail.com>
2024-01-18 14:53:21 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240118/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -5,6 +5,6 @@ two_cnt = 0
 for i, elem in enumerate(arr):
     if elem == 2:
         two_cnt += 1
-        if two_cnt == 3:
-            print(i+1)
-            break
+    if two_cnt == 3:
+        print(i+1)
+        break
