3172ea25bb0f4767c5ccfd0570ce82812304361a
April Dayae Keum <dayeyae@gmail.com>
2024-01-18 14:33:42 +0900

    Title: 전항의 두 배, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/전항의 두 배/twice-the-previous.py
+++ 240118/전항의 두 배/twice-the-previous.py
@@ -2,7 +2,7 @@ first, second = list(map(int, input().split()))
 n = 10
 
 print(first, second, end=' ')
-for i in range(n - 2):
+for i in range(2, 10):
     curr = (first*2) + second
     print(curr, end=' ')
     first = second
