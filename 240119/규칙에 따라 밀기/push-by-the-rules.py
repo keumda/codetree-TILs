6d1b18a65e139f29000587668c0cf16d49a8408a
April Dayae Keum <dayeyae@gmail.com>
2024-01-19 20:56:30 +0900

    Title: 규칙에 따라 밀기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/규칙에 따라 밀기/push-by-the-rules.py
+++ 240119/규칙에 따라 밀기/push-by-the-rules.py
@@ -4,14 +4,7 @@ command = input()
 total_step_cnt = 0
 for c in command:
     if c == 'L':
-        total_step_cnt -= 1
+        a = a = a[1:] + a[:1]
     if c == 'R':
-        total_step_cnt += 1
-
-if total_step_cnt < 0:
-    abs_step_cnt = abs(total_step_cnt)
-    a = a[abs_step_cnt:] + a[:abs_step_cnt]
-else:
-    a = a[:total_step_cnt] + a[total_step_cnt:]
-
+        a = a = a[-1:] + a[:-1]
 print(a)
