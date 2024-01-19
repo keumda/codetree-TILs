c73448638a2f1ac0d2c4cacb55ca10b8e01469e1
April Dayae Keum <dayeyae@gmail.com>
2024-01-19 20:46:10 +0900

    Title: 규칙에 따라 밀기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240119/규칙에 따라 밀기/push-by-the-rules.py
@@ -0,0 +1,17 @@
+a = input()
+command = input()
+
+total_step_cnt = 0
+for c in command:
+    if c == 'L':
+        total_step_cnt -= 1
+    if c == 'R':
+        total_step_cnt += 1
+
+if total_step_cnt < 0:
+    abs_step_cnt = abs(total_step_cnt)
+    a = a[abs_step_cnt:] + a[:abs_step_cnt]
+else:
+    a = a[:abs_step_cnt] + a[abs_step_cnt:]
+
+print(a)
