1c4cf082b6f802875db436461bd9337c0be1a561
April Dayae Keum <dayeyae@gmail.com>
2024-01-19 17:52:58 +0900

    Title: 가장 짧은 문자열, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/가장 짧은 문자열/shortest-string.py
+++ 240119/가장 짧은 문자열/shortest-string.py
@@ -1,8 +1,11 @@
 min_size = 20
+max_size = 0
 
 for _ in range(3):
     curr_str = input()
     if len(curr_str) < min_size:
         min_size = len(curr_str)
+    if len(curr_str) > max_size:
+        max_size = len(curr_str)
 
-print(min_size)
+print(max_size - min_size)
