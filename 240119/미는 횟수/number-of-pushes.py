79a667a4646cc5a433ddddc54b0b34bb2cf28300
April Dayae Keum <dayeyae@gmail.com>
2024-01-19 23:10:40 +0900

    Title: 미는 횟수, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/미는 횟수/number-of-pushes.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+
+min_step = 0
+for i in range(1, len(a)):
+    temp_a = a[-i:] + a[:len(a)-i]
+    if temp_a == b:
+        min_step = i
+        break
+
+print(min_step)
