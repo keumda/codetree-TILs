74e46b44aac027c22d92635064d24a24617b83da
April Dayae Keum <dayeyae@gmail.com>
2024-01-20 17:15:30 +0900

    Title: 최소공배수 구하기, Time: 185ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240120/최소공배수 구하기/find-the-least-common-multiple.py
@@ -3,12 +3,14 @@ n, m = tuple(map(int, input().split()))
 def find_common_divider(n, m):
     arr = []
     min_num = min(n, m)
-    for i in range(2, min_num+1):
+    s = 1
+    for i in range(min_num, 1, -1):
         if n%i == 0 and m%i == 0:
-            arr.append(i)
-    return arr
+            s = i
+            break
+    return s
 
-s = 1
-for elem in find_common_divider(n, m):
-    s *= elem
-print(s)
+
+cd = find_common_divider(n, m)
+cm = n//cd * m//cd * cd
+print(cm)
