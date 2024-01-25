7fdc02396480004e3080fdb2ada615999ffac018
April Dayae Keum <dayeyae@gmail.com>
2024-01-25 23:15:54 +0900

    Title: 100으로 나눈 나머지의 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240125/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+def find_nth(first, second, n, cnt):
+    if n == 1:
+        curr = 2
+    if n == 2:
+        curr = 4
+    curr = first * second % 100
+    first = second
+    second = curr
+    if cnt == n:
+        return curr
+    else:
+        return find_nth(first, second, n, cnt+1)
+    
+print(find_nth(2, 4, n, 3))
