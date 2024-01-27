a12eb49e34d426b1fd7c89ab779ffad65acf801d
April Dayae Keum <dayeyae@gmail.com>
2024-01-28 04:42:04 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240128/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240128/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -1,15 +1,13 @@
 n = int(input())
 seq = list(map(int, input().split()))
 arr = []
-arr2 = []
+answer = [0]*n
 for i, elem in enumerate(seq):
-    arr.append((i+1, elem))
+    arr.append((i, elem))
 arr.sort(key=lambda x:x[1])
 
 for i, elem in enumerate(arr):
-    prev_num, val = elem
-    arr2.append((i+1, prev_num, val))
-arr2.sort(key=lambda x:x[1])
+    answer[elem[0]] = i + 1
 
-for elem in arr2:
-    print(elem[0], end=' ')
+for idx in answer:
+    print(idx, end=' ')
