143ccfbc10ea00c381875bec3af017eb9ab86130
April Dayae Keum <dayeyae@gmail.com>
2024-01-28 04:02:15 +0900

    Title: 줄 세우기, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240128/줄 세우기/line-up-students.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = []
+for i in range(n):
+    h, w = tuple(map(int, input().split()))
+    student = (h, w, i+1)
+    arr.append(student)
+
+arr.sort(key=lambda x:(-x[0], -x[1], x[2]))
+
+for elem in arr:
+    print(elem[0], elem[1], elem[2])
