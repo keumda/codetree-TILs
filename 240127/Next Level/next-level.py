023b06d99876cd160b3dd7dd6dfa789a926403a6
April Dayae Keum <dayeyae@gmail.com>
2024-01-27 14:00:02 +0900

    Title: Next Level, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/Next Level/next-level.py
+++ 240127/Next Level/next-level.py
@@ -1,11 +1,13 @@
 class Student():
-    def __init__(self, id, level='1'):
-        self.id = id
+    def __init__(self, user_id='', level='1'):
+        self.user_id = user_id
         self.level = level
 
 b_id, b_level = input().split()
 a = Student('codetree', '10')
-b = Student(b_id, b_level)
+b = Student()
+b.user_id = b_id
+b.level = b_level
 
-print(f'user {a.id} lv {a.level}')
-print(f'user {b.id} lv {b.level}')
+print(f'user {a.user_id} lv {a.level}')
+print(f'user {b.user_id} lv {b.level}')
