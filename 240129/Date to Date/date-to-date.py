c4046a652bba687c657875dff595e09b262df763
April Dayae Keum <dayeyae@gmail.com>
2024-01-29 07:36:57 +0900

    Title: Date to Date, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/Date to Date/date-to-date.py
+++ 240129/Date to Date/date-to-date.py
@@ -3,11 +3,11 @@ m, d, m1, d1 = tuple(map(int, input().split()))
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 m_days = d
-for i in range(m+1):
+for i in range(m):
     m_days += month_days[i]
 
 m1_days = d1
-for i in range(m1+1):
+for i in range(m1):
     m1_days += month_days[i]
 
-print(m1_days - m_days)
+print(m1_days - m_days + 1)
