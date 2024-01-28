25fde2de48a4ddd45d5b771cf37c3cd1b93c590b
April Dayae Keum <dayeyae@gmail.com>
2024-01-29 07:33:26 +0900

    Title: Date to Date, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240129/Date to Date/date-to-date.py
+++ 240129/Date to Date/date-to-date.py
@@ -3,15 +3,11 @@ m, d, m1, d1 = tuple(map(int, input().split()))
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 m_days = d
-for i, d in enumerate(month_days):
-    if i == m:
-        break
-    m_days += d
+for i in range(m+1):
+    m_days += month_days[i]
 
 m1_days = d1
-for i, d in enumerate(month_days):
-    if i == m1:
-        break
-    m1_days += d
+for i in range(m1+1):
+    m1_days += month_days[i]
 
-print(m1_days - m_days + 1)
+print(m1_days - m_days)
