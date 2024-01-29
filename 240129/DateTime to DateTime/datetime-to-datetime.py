23c9932cdf61a4f0504f5a192655190be5ba7e9e
April Dayae Keum <dayeyae@gmail.com>
2024-01-29 10:26:07 +0900

    Title: DateTime to DateTime, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/DateTime to DateTime/datetime-to-datetime.py
+++ 240129/DateTime to DateTime/datetime-to-datetime.py
@@ -6,5 +6,10 @@ def calc_min(d, h, m):
         total_min += 24 * 60
     return total_min
 
+# 2011년 11월 1일 0시 0분 기준
 a, b, c = tuple(map(int, input().split()))
-print(calc_min(a, b, c)-calc_min(11, 11, 11))
+diff = calc_min(a, b, c)-calc_min(11, 11, 11)
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
