82cc1140b731172be0385fe9018f93a2d9df9ea8
April Dayae Keum <dayeyae@gmail.com>
2024-01-30 22:11:31 +0900

    Title: 신기한 타일 뒤집기, Time: 155ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240130/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240130/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,18 +1,18 @@
 n = int(input())
 
-slots = ['N']*100000
-start_idx = 50000
+slots = ['N']*200001
+start_idx = 100000
 for _ in range(n):
     x_str, direction = tuple(input().split())
     x = int(x_str)
     if direction == 'R':
         for i in range(start_idx, start_idx+x):
             slots[i] = 'B'
-        start_idx = start_idx + x - 1
+        start_idx = start_idx + x
     if direction == 'L':
         for i in range(start_idx-1, start_idx-x-1, -1):
             slots[i] = 'W'
-        start_idx = start_idx - x\
+        start_idx = start_idx - x
 
 b_cnt = 0
 w_cnt = 0
