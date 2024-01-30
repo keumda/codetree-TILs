96b391ae5e3954f3fd88688f7cd7c0ec35899f38
April Dayae Keum <dayeyae@gmail.com>
2024-01-30 21:51:31 +0900

    Title: 왔다 갔던 구역 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240130/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,15 +7,18 @@ for _ in range(n):
     x, direction = tuple(input().split())
     x = int(x)
     if direction == 'L':
-        for i in range(start_idx, start_idx-x, -1):
+        for i in range(start_idx-1, start_idx-x, -1):
             slots[i] += 1
-        start_idx -= x
+        start_idx = start_idx - x
+        # print(slots[985:1005])
     if direction == 'R':
-        for i in range(start_idx, start_idx+x):
+        for i in range(start_idx+1, start_idx+x+1):
             slots[i] += 1
-        start_idx += x
+        start_idx = start_idx + x + 1
+        # print(slots[985:1005])
     
 twice_cnt = 0
+# print(slots[985:1005])
 for elem in slots:
     if elem >= 2:
         twice_cnt += 1
