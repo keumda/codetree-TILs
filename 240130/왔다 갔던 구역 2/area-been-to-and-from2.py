5207a4a91716af8673e91a8c9f4f8be2ee0d2280
April Dayae Keum <dayeyae@gmail.com>
2024-01-30 22:07:29 +0900

    Title: 왔다 갔던 구역 2, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240130/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240130/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,19 +7,23 @@ for _ in range(n):
     x, direction = tuple(input().split())
     x = int(x)
     if direction == 'L':
-        for i in range(start_idx-x, start_idx):
+        for i in range(start_idx-1, start_idx-x-1, -1):
             slots[i] += 1
         start_idx -= x
-        # print(slots[985:1005])
     if direction == 'R':
         for i in range(start_idx, start_idx+x):
             slots[i] += 1
         start_idx += x
-        # print(slots[985:1005])
     
 twice_cnt = 0
-# print(slots[985:1005])
 for elem in slots:
     if elem >= 2:
         twice_cnt += 1
 print(twice_cnt)
+
+
+# for i in range(2, 5):
+#     print(i, end=' ')
+# print()
+# for j in range(5-1, 2-1, -1):
+#     print(j, end=' ')
