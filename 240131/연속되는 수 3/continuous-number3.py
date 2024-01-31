061a9bab92f4a9cecdb4478d20e152a712b8a0dc
April Dayae Keum <dayeyae@gmail.com>
2024-01-31 21:23:14 +0900

    Title: 연속되는 수 3, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/연속되는 수 3/continuous-number3.py
+++ 240131/연속되는 수 3/continuous-number3.py
@@ -3,14 +3,14 @@ n = int(input())
 cnt = 1
 max_cnt = 0
 prev = int(input())
-for _ in range(n-1):
+for i in range(n-1):
     curr = int(input())
     # print(prev, curr)
     if prev < 0 and curr < 0:
         cnt += 1
     if prev > 0 and curr > 0:
         cnt += 1
-    if (prev > 0 and curr < 0) or (prev < 0 and curr > 0):
+    if (prev > 0 and curr < 0) or (prev < 0 and curr > 0) or i == n-2:
         if cnt > max_cnt:
             max_cnt = cnt
         cnt = 1
