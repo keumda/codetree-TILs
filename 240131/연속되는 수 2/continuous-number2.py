1132af16899a734b408f2aed954de82692f6d8b1
April Dayae Keum <dayeyae@gmail.com>
2024-01-31 21:26:37 +0900

    Title: 연속되는 수 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/연속되는 수 2/continuous-number2.py
+++ 240131/연속되는 수 2/continuous-number2.py
@@ -1,12 +1,16 @@
 n = int(input())
 
 cnt = 1
+max_cnt = 1
 prev = 0
-for _ in range(n):
+for i in range(n):
     curr = int(input())
     # print(prev, curr, cnt)
-    if prev != curr:
+    if prev != curr or i == n-1:
         prev = curr
+        if max_cnt < cnt:
+            max_cnt = cnt
     else:
         cnt += 1
+    
 print(cnt)
