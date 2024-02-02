22e51e19f6e0b115a674f4748ec734e469f91ad4
April Dayae Keum <dayeyae@gmail.com>
2024-02-03 00:56:10 +0900

    Title: 1이 3개 이상 있는 위치, Time: 140ms, Memory: 26MB, Status: Passed - CodeTree

--- 240203/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240203/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -8,17 +8,20 @@ for _ in range(n):
 def in_range(col, row):
     return col >= 0 and col < n and row >= 0 and row < n
 
+def adj_cnt(i, j):
+    cnt = 0
+    for di, dj in zip(dis, djs):
+        check_i, check_j = i + di, j + dj
+        # print(check_i, check_j)
+        if in_range(check_i, check_j) and arr[check_i][check_j]==1:
+            cnt += 1
+    return cnt
+
 dis, djs = [1, 0, -1, 0], [0, 1, 0, -1]
 
 more_than_three_cnt = 0
 for i, row in enumerate(arr):
     for j, col in enumerate(row):
-        cnt = 0
-        for di, dj in zip(dis, djs):
-            check_i, check_j = i + di, j + dj
-            # print(check_i, check_j)
-            if in_range(check_i, check_j) and arr[check_i][check_j]==1:
-                cnt += 1
-        if cnt >= 3:
+        if adj_cnt(i, j) >= 3:
             more_than_three_cnt += 1
 print(more_than_three_cnt)
