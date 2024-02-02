4b7bba38e085f90cb6d3b94bb4a020cd6e0f928c
April Dayae Keum <dayeyae@gmail.com>
2024-02-02 19:46:48 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240202/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,27 @@
+total_n, k, infected_n, handshake_t = tuple(map(int, input().split()))
+
+devlopers_k = [0]*(total_n+1)
+devlopers_k[infected_n] = k
+devlopers_infected = [0]*(total_n+1)
+devlopers_infected[infected_n] = 1
+
+t_arr = []
+for _ in range(handshake_t):
+    t = tuple(map(int, input().split()))
+    t_arr.append(t)
+t_arr.sort(key = lambda x:x[0])
+
+for elem in t_arr:
+    t, x, y = elem
+    if devlopers_infected[x] == 1 and devlopers_k[x] > 0:
+        devlopers_k[x] -= 1
+        devlopers_infected[y] = 1
+        devlopers_k[y] = k
+    elif devlopers_infected[y] == 1 and devlopers_k[y] > 0:
+        devlopers_k[y] -= 1
+        devlopers_infected[x] = 1
+        devlopers_k[x] = k
+    # print(devlopers_infected, devlopers_k)
+
+for elem in devlopers_infected[1:]:
+    print(elem, end='')
