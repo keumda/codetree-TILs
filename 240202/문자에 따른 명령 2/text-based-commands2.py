b65a2cfb8d883f082f9703a2c01a226912246f64
April Dayae Keum <dayeyae@gmail.com>
2024-02-02 23:00:53 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,17 @@
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+cur_d_idx = 0
+x, y = 0, 0
+
+path = input()
+for i in path:
+    if i == 'L':
+        cur_d_idx -= 1
+    if i == 'R':
+        cur_d_idx += 1
+    if i == 'F':
+        if cur_d_idx < 0:
+            x, y = x + dx[cur_d_idx], y + dy[cur_d_idx]
+        else:
+            x, y = x + dx[cur_d_idx], y + dy[cur_d_idx]
+print(x, y)
