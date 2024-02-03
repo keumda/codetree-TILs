bad8be6428fb02eafa0398b2cc7a3b4a28463198
April Dayae Keum <dayeyae@gmail.com>
2024-02-03 22:37:17 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240203/되돌아오기 2/come-back-2.py
@@ -0,0 +1,20 @@
+command = input()
+
+x, y = 0, 0
+t = 0
+res = -1
+d = ['N', 'E', 'S', 'W']
+d_idx = 0
+dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+for c in command:
+    if c == 'R':
+        d_idx = d_idx + 1 % 4
+    if c == 'L':
+        d_idx = d_idx - 1 % 4
+    if c == 'F':
+        x, y = x + dx[d_idx], y + dy[d_idx]
+    t += 1
+    if x == 0 and y == 0:
+        res = t
+        break
+print(res)
