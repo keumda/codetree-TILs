7f5584210cec504df8e9b83e3a0b919cc13832d1
April Dayae Keum <dayeyae@gmail.com>
2024-02-03 14:42:52 +0900

    Title: 되돌아오기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/되돌아오기/come-back.py
+++ 240203/되돌아오기/come-back.py
@@ -9,22 +9,24 @@ mapper = {
 di, dj = [0, 1, 0, -1], [1, 0, -1, 0]
 total_t = 0
 i, j = 0, 0
-
-def find_time(d, t, i, j, total_t):
-    res = -1
-
-
 res = -1
-for _ in range(n):
-    d, t = tuple(input().split())
-    t = int(t)
+
+def move(d, t):
+    global i, j, res, total_t
     for sec in range(t):
         i, j = i + di[mapper[d]], j + dj[mapper[d]]
         total_t += 1
         if i == 0 and j == 0:
             res = total_t
-            break
-    if i == 0 and j == 0:
+            return True
+    return False
+
+
+for _ in range(n):
+    d, t = tuple(input().split())
+    t = int(t)
+    done = move(d, t)
+    if done:
         break
     
 print(res)
