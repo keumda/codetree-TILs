ab19d8a53fdd940a7914be7ce94eb515a7cd1639
April Dayae Keum <dayeyae@gmail.com>
2024-02-05 03:48:12 +0900

    Title: 체크판위에서 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240205/체크판위에서 2/on-the-checkboard-2.py
+++ 240205/체크판위에서 2/on-the-checkboard-2.py
@@ -4,14 +4,19 @@ for _ in range(r):
     row = list(input().split())
     arr.append(row)
 
-curr_c = arr[0][0]
+first_c = arr[0][0]
+if first_c == 'W':
+    second_c = 'B'
+    third_c = 'W'
+else:
+    second_c = 'W'
+    third_c = 'B'
 cnt = 0
-for i in range(1, r-1):
-    for j in range(1, c-1):
-        if curr_c != arr[i][j]:
-            curr_c = arr[i][j]
-            for k in range(i+1, r-1):
-                for l in range(j+1, c-1):
-                    if curr_c != arr[k][l]:
-                        cnt += 1     
+for i in range(1, r):
+    for j in range(1, c):
+        if arr[i][j] == second_c:
+            for k in range(i+1, r):
+                for l in range(j+1, c):
+                    if arr[k][l] == third_c and third_c != arr[r-1][c-1]:
+                        cnt += 1
 print(cnt)
