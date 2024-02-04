4d96ff3ba77cdff05888e0a0a8a2c25b4477e633
April Dayae Keum <dayeyae@gmail.com>
2024-02-05 02:48:49 +0900

    Title: 일렬로 서있는 소 2, Time: 161ms, Memory: 28MB, Status: Passed - CodeTree

--- 240205/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240205/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ cnt = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if heights[i] < heights[j] and heights[j] < heights[k]:
+            if heights[i] <= heights[j] and heights[j] <= heights[k]:
                 cnt += 1
 
 print(cnt)
