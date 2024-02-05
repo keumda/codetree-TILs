4b2ba3724d2cf7918c8804ca94e3ba5e92c1e0a7
April Dayae Keum <dayeyae@gmail.com>
2024-02-05 15:02:29 +0900

    Title: 이상한 진수 2, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,16 @@
+n = input()
+
+def convert_binary(num):
+    coverted = 0
+    for i in num:
+        coverted = (coverted * 2) + int(i)
+    return coverted
+
+max_num = n
+for i in range(1, len(n)):
+    changed_n = list(n)
+    changed_n[i] = str(abs(int(n[i]) - 1))
+    changed_n = ''.join(changed_n)
+    if convert_binary(changed_n) > convert_binary(max_num):
+        max_num = changed_n
+print(convert_binary(max_num))
