409871fb4a753a1871dae997718bc00b5ae6b385
April Dayae Keum <dayeyae@gmail.com>
2024-02-07 00:19:14 +0900

    Title: 밭의 높이를 고르게하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,12 @@
+import sys
+
+n, h, t = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+min_cost = sys.maxsize
+for i in range(n-t):
+    cost = 0
+    for j in range(i, i+t):
+        cost += abs(h - arr[j])
+    min_cost = min(min_cost, cost)
+    
+print(min_cost)
