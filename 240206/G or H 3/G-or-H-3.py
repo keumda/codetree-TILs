9b52f22120fa0d9d04c2e0fd6baafd235d7fa138
April Dayae Keum <dayeyae@gmail.com>
2024-02-06 23:21:15 +0900

    Title: G or H 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240206/G or H 3/G-or-H-3.py
@@ -0,0 +1,22 @@
+n, k = tuple(map(int, input().split()))
+commands = [
+    tuple(input().split())
+    for row in range(n)
+]
+arr_l = max(commands, key=lambda item: int(item[0]))
+arr = [0]*(int(arr_l[0])+1)
+
+for c in commands:
+    # print(c)
+    idx = int(c[0])
+    arr[idx] = c[1]
+max_score = 0
+for i in range(1, len(arr)-k):
+    score = 0
+    for j in range(i, i+k+1):
+        if arr[j] == 'G':
+            score += 1
+        if arr[j] == 'H':
+            score += 2
+    max_score = max(score, max_score)
+print(max_score)
