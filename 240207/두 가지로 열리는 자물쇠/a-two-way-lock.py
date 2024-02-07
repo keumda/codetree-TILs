4742f8d8ffc1b8eeb8277d9a033030cfbc61a416
April Dayae Keum <dayeyae@gmail.com>
2024-02-07 18:49:39 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 207ms, Memory: 28MB, Status: Passed - CodeTree

--- 240207/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240207/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -4,8 +4,8 @@ x,y,z = tuple(map(int, input().split()))
 
 cnt = 0
 for i in range(1, n+1):
-    for j in range(i+1, n+1):
-        for k in range(j+1, n+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):
             if (abs(a - i) <= 2 or abs(a - i) >= n - 2) \
                 and (abs(b - j) <= 2 or abs(b - j) >= n - 2) \
                 and (abs(c - k) <= 2 or abs(c - k) >= n - 2):
