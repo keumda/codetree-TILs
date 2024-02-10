0febc553fb81c882a020515a01ebbb5eb35b23b0
April Dayae Keum <dayeyae@gmail.com>
2024-02-11 04:26:42 +0900

    Title: 스승의 은혜 3, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240211/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240211/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -5,18 +5,20 @@ for i in range(n):
     s = [i+1]
     s= s + list(map(int, input().split()))
     arr.append(s)
+# print(arr)
 arr.sort(key = lambda x:x[1]+x[2])
+# print(arr)
 
 max_cnt = 0
 for i in range(n):
-    price = arr[i][1]//2 + arr[i][2]
     cnt = 0
     for j in range(n):
         if i != j:
-            if price > b:
-                # print(i, j)
-                cnt = arr[j-1][0]
-                break
             price += arr[j][1] + arr[j][2]
+        else:
+            price = arr[i][1]//2 + arr[i][2]
+        if price > b:
+            cnt = arr[j][0]
+            break
     max_cnt = max(cnt, max_cnt)
 print(max_cnt)
