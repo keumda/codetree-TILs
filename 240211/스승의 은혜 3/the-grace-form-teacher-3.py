c240ae3eac615f921d45b5be8873d6c08202bd27
April Dayae Keum <dayeyae@gmail.com>
2024-02-11 04:50:12 +0900

    Title: 스승의 은혜 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240211/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240211/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,25 +1,34 @@
 n, b = tuple(map(int, input().split()))
-arr = []
-
-for i in range(n):
-    s = [i+1]
-    s= s + list(map(int, input().split()))
-    arr.append(s)
-# print(arr)
-arr.sort(key = lambda x:x[1]+x[2])
+arr = [
+    tuple(map(int, input().split()))
+    for r in range(n)
+]
+arr.sort(key = lambda x:(x[0]+x[1]))
 # print(arr)
 
 max_cnt = 0
 for i in range(n):
     cnt = 0
     price = 0
-    for j in range(n):
-        if i != j:
-            price += arr[j][1] + arr[j][2]
+    # others = arr[:i]+arr[i+1:]
+    # print(others)
+    for j, (p, s) in enumerate(arr):
+        if j == i:
+            price += (p//2 + s)
         else:
-            price += arr[i][1]//2 + arr[i][2]
+            price += (p + s)
         if price <= b:
-            # print(i, j, price, arr[j])
             cnt += 1
-    max_cnt = max(cnt, max_cnt)
+    max_cnt = max(max_cnt, cnt)
+
+
+    # for j in range(n):
+    #     if i != j:
+    #         price += arr[j][1] + arr[j][2]
+    #     else:
+    #         price += arr[i][1]//2 + arr[i][2]
+    #     if price <= b:
+    #         # print(i, j, price, arr[j])
+    #         cnt += 1
+    # max_cnt = max(cnt, max_cnt)
 print(max_cnt)
