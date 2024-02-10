c7cfe517b7ad4dba86cb5e3ae869296c8e2371f9
April Dayae Keum <dayeyae@gmail.com>
2024-02-10 18:52:55 +0900

    Title: 이상한 폭탄 2, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240210/이상한 폭탄 2/strange-bomb-2.py
+++ 240210/이상한 폭탄 2/strange-bomb-2.py
@@ -6,7 +6,8 @@ bombs = [
 max_bomb = -1
 for i in range(n):
     for j in range(i+1, n):
-        # print(bombs[i], bombs[j], abs(bombs[i]-bombs[j]))
-        if bombs[i] == bombs[j] and abs(bombs[i]-bombs[j]) <= k:
+        
+        if bombs[i] == bombs[j] and abs(i-j) <= k:
+            # print(bombs[i], bombs[j], abs(i-j))
             max_bomb = max(bombs[i], bombs[j], max_bomb)
 print(max_bomb)
