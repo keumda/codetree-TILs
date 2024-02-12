a41b08510a8119cc14a692eb0a556db823430e7c
April Dayae Keum <dayeyae@gmail.com>
2024-02-13 04:10:11 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240213/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -3,8 +3,8 @@ a, b, c = tuple(map(int, input().split()))
 max_a = c // a
 max_b = c // b
 max_s = 0
-for i in range(max_a):
-    for j in range(max_b):
+for i in range(max_a+1):
+    for j in range(max_b+1):
         # print(i, j)
         s = (i*a) + (j*b)
         if s <= c:
