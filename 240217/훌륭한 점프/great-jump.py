f448a8b38e40e7366d2a7d2371ab8020e53b00e0
April Dayae Keum <dayeyae@gmail.com>
2024-02-17 08:41:59 +0900

    Title: 훌륭한 점프, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240217/훌륭한 점프/great-jump.py
+++ 240217/훌륭한 점프/great-jump.py
@@ -1,24 +1,24 @@
 n, k = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-def is_possible(min_val):
-    available_indices = []
-    for i, elem in enumerate(arr):
-        if elem >= min_val:
-            available_indices.append(i)
-
-    arr_size = len(available_indices)
-    for i in range(1, arr_size):
-        dist = available_indices[i] - available_indices[i - 1]
+def is_possible(max_val):
+    available_idx = []
+    for idx, elem in enumerate(arr):
+        if elem <= max_val:
+            available_idx.append(idx)
+    if len(available_idx) <= 1:
+        return False
+    # print(max_val, available_idx)
+    for j in range(1, len(available_idx)):
+        dist = available_idx[j] - available_idx[j-1]
         if dist > k:
             return False
-
     return True
 
 
-maximin = 0
-for a in range(1, n):
-    # print(a)
-    if is_possible(a):
-        maximin = max(maximin, a)
-print(maximin)
+
+minmax = 10000
+for i in range(max(arr)+1):
+    if is_possible(i):
+        minmax = min(minmax, i)
+print(minmax)
