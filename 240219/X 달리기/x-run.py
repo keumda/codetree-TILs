a5d292e90720f0f4304ee0194397d4981888e3b9
April Dayae Keum <dayeyae@gmail.com>
2024-02-19 01:43:57 +0900

    Title: X 달리기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240219/X 달리기/x-run.py
+++ 240219/X 달리기/x-run.py
@@ -3,12 +3,23 @@ dist = int(input())
 t = 0
 v = 0
 total_d = 0
+increase = 0
 while True:
-    # print(t, v, total_d)
-    v += 1
+    if total_d >= dist:
+        break
+    if increase == 0:
+        v += 1
+    elif increase == 1:
+        v -= 1
+    else:
+        v = v
     total_d += v
     t += 1
-    if total_d > dist//2:
-        break
+    # print(t, v, total_d)
+    if total_d >= dist//2:
+        increase = 1
+    if v == 1 and total_d == (dist - 1):
+        increase = 2
+
     
-print(t+v)
+print(t)
