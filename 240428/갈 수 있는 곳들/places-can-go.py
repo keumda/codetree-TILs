bbe8b6dd1c27be6d5144bd41efd1036927d3ffab
April Dayae Keum <dayeyae@gmail.com>
2024-04-28 01:08:02 +0900

    Title: 갈 수 있는 곳들, Time: 249ms, Memory: 27MB, Status: Passed - CodeTree

--- 240428/갈 수 있는 곳들/places-can-go.py
+++ 240428/갈 수 있는 곳들/places-can-go.py
@@ -13,8 +13,6 @@ visited = [
     [False for _ in range(n)]
     for _ in range(n)
 ]
-
-
 q = deque()
 
 def in_range(x, y):
@@ -35,26 +33,20 @@ def bfs():
         x, y = q.popleft()
         for dx, dy in zip(dxs, dys):
             new_x, new_y = x + dx, y + dy
-            # print(new_x, new_y)
             if can_go(new_x, new_y):
                 visited[new_x][new_y] = True
-                # total_visited[new_x][new_y] = True
                 q.append([new_x, new_y])
 
 for st_x, st_y in ks:
     st_x, st_y = st_x - 1, st_y - 1
 
     visited[st_x][st_y] = True
-    # total_visited[st_x][st_y] = True
     q.append([st_x, st_y])
-    # print(st_x, st_y)
     bfs()
-    # print(visited)
 
 cnt = 0
 for i in range(n):
     for j in range(n):
-        # if total_visited[i][j]:
         if visited[i][j]:
             cnt += 1
 print(cnt)
