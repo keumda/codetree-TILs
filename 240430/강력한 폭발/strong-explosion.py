7c69e18e88143fa7387a8a0a7ac40432fd4b2714
April Dayae Keum <dayeyae@gmail.com>
2024-04-30 03:45:42 +0900

    Title: 강력한 폭발, Time: 386ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240430/강력한 폭발/strong-explosion.py
@@ -0,0 +1,77 @@
+n = int(input())
+grid = [
+    input().split()
+    for _ in range(n)
+]
+bomb_type = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+visited = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
+pts = []
+max_cnt = 0
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == "1":
+            pts.append([i, j])
+
+def print_visited():
+    for v in visited:
+        for c in v:
+            print(c, end=" ")
+        print()
+    print("======")
+
+def count():
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                visited[i][j] = False
+    for i in range(n):
+        for j in range(n):
+            if bomb_type[i][j]:
+                fill_grid(i, j, bomb_type[i][j])
+    # print_visited()
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                cnt += 1
+
+    global max_cnt
+    max_cnt = max(max_cnt, cnt)
+
+
+def in_range(i, j):
+    return i >= 0 and i < n and j >= 0 and j < n
+
+def choose(n):
+    if n == len(pts):
+        count()
+        return
+    for i in range(3):
+        x, y = pts[n]
+        bomb_type[x][y] = i
+        choose(n + 1)
+        bomb_type[x][y] = 0
+
+
+def fill_grid(i, j, k):
+    if k == 0:
+            dis, djs = [-2, -1, 1, 2], [0, 0, 0, 0]
+    if k == 1:
+            dis, djs = [-1, 1, 0, 0], [0, 0, -1, 1]
+    if k == 2:
+            dis, djs = [-1, -1, 1, 1], [-1, 1, -1, 1]
+    visited[i][j] = True
+    for di, dj in zip(dis, djs):
+        new_i, new_j = i + di, j + dj
+        if in_range(new_i, new_j):
+            visited[new_i][new_j] = True
+
+choose(0)
+
+print(max_cnt)
