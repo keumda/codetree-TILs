729b0b0e3bec1e4694cb28f94af4b00d5194fc6b
April Dayae Keum <dayeyae@gmail.com>
2024-05-05 19:39:36 +0900

    Title: 알파벳과 사칙연산, Time: 218ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240505/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,49 @@
+formula = input()
+
+alph = []
+match_alph = ['a', 'b', 'c', 'd', 'e', 'f']
+alpha_map =  {'a': 0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0}
+res = list()
+
+def choose(idx):
+    if idx == len(match_alph):
+        calculate()
+        return
+    else:
+        for j in range(1, 4 + 1):
+            alpha_map[match_alph[idx]] = j
+            choose(idx + 1)
+        return
+
+def calculate():
+    idx = 0
+    curr = formula[idx]
+    while True:
+        
+        sigh = formula[idx+1]
+        second = formula[idx+2]
+
+        if type(curr) is str:
+            curr = alpha_map[curr]
+        if sigh == '*':
+            val = curr * alpha_map[second]
+        if sigh == '+':
+            val = curr + alpha_map[second]
+        if sigh == '-':
+            val = curr - alpha_map[second]
+        # if alpha_map['a'] == 4 and alpha_map['b'] == 4 and alpha_map['c'] == 1:
+        #     print(curr, sigh, second, val)
+        curr = val
+        idx += 1
+
+        if idx + 2 >= len(formula):
+            # if curr >= 12:
+            #     print(alpha_map)
+            res.append(curr)
+            break
+        
+if len(formula) == 1:
+    print(4)
+else:
+    choose(0)
+    print(max(res))
