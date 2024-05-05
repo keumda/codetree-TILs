560c5d27296ce7660f0418087f19568523c0ae09
April Dayae Keum <dayeyae@gmail.com>
2024-05-06 03:43:24 +0900

    Title: 최고의 33위치, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240506/최고의 33위치/best-place-of-33.py
+++ 240506/최고의 33위치/best-place-of-33.py
@@ -4,20 +4,21 @@ grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
+m = 3
 
-def get_sum(grid):
+def get_sum(i, j):
     s = 0
-    for row in grid:
-        for item in row:
-            s += item
+    for x in range(i, i+m):
+        for y in range(j, j+m):
+            s += grid[x][y]
     return s
 
 
 max_cnt = 0
-m = 3
 for i in range(n - m + 1):
     for j in range(n - m + 1):
-        s = get_sum(grid[i:i+m][j:j+m])
+        s = get_sum(i, j)
         max_cnt = max(max_cnt, s)
 
 print(max_cnt)
+# get_sum(0, 0)
