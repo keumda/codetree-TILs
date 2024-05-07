0f9844ee30b229c226f06c2d28d6191f337a2900
April Dayae Keum <dayeyae@gmail.com>
2024-05-07 18:28:36 +0900

    Title: 금 채굴하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/금 채굴하기/gold-mining.py
+++ 240507/금 채굴하기/gold-mining.py
@@ -39,14 +39,22 @@ def get_num_of_gold(row, col, k):
 
 max_gold = 0
 
+total_gold = 0
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            total_gold += 1
+max_k = 0
+max_digging_price = total_gold * m
+while True:
+    if get_area(max_k) >= max_digging_price:
+        break
+    max_k += 1
+
 # 격자의 각 위치가 마름모의 중앙일 때 채굴 가능한 금의 개수를 구합니다.
 for row in range(n):
     for col in range(n):
-        for k in range(2*n):
+        for k in range(max_k):            
             num_of_gold = get_num_of_gold(row, col, k)
-            
-            # 손해를 보지 않으면서 채굴할 수 있는 최대 금의 개수를 저장합니다.
-            if num_of_gold * m >= get_area(k):
-                max_gold = max(max_gold, num_of_gold)
-
+            max_gold = max(max_gold, num_of_gold)
 print(max_gold)
